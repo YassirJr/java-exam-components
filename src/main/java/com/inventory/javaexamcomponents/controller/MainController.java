74bc65bb3abee8f61e389956f258bd46b944165c
YassirJr <yassir.junior.03@gmail.com>
2025-01-04 15:48:48 +0100

    add main view

---
+++ src/main/java/com/inventory/javaexamcomponents/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.inventory.javaexamcomponents.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
+
+public class MainController {
+    @FXML
+    private TabPane mainTabPane;
+
+    @FXML
+    private void showProfesseurs() {
+        mainTabPane.getSelectionModel().select(0);
+    }
+
+    @FXML
+    private void showDepartements() {
+        mainTabPane.getSelectionModel().select(1);
+    }
+}
